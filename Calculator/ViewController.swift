cac0f130c2357fd8073fd52d408a1adf9ba5dbf8
Rob Harting <robharting@hotmail.com>
2024-10-21 08:45:30 +0200

    add AC +/_ and % functionality

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,18 +12,53 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     
+    private var isFinishedTypingNumber: Bool = true
     
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
-    
+                
+        isFinishedTypingNumber = true
+        
+        guard let number = Double(displayLabel.text!) else {
+            fatalError("Cannot convert display label text to a Double Rob.")
+        }
+        
+        if let calcMethod = sender.currentTitle  {
+            if calcMethod == "AC" {
+                displayLabel.text = "0"
+            } else if calcMethod == "+/-" {
+                displayLabel.text = String(number * -1)
+            } else if calcMethod == "%" {
+                displayLabel.text = String(number / 100)
+            }
+        }
+        
+       
     }
 
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
         //What should happen when a number is entered into the keypad
+        
+        
+        if let numValue = sender.currentTitle {
+            
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+               
+                isFinishedTypingNumber = false
+            } else {
+                displayLabel.text = displayLabel.text! + numValue
+            }
+            
+            
+            
+        }
+            
+        
     
     }
 
